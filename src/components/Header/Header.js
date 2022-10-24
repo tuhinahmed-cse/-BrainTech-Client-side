1435de9919f314ee6dfb88dbb4c0ece461be3099
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-10-24 23:33:52 +0600

    title and logo change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { FaBrain } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
         <Navbar className='mb-4' collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand ><Link to='/' style={{textDecoration:'none'}} ><FaBrain style={{color:'tomato', height:'30px'}}></FaBrain> <span style={{color:'goldenrod'}}>Brain</span><span className='text-info'>Tech</span> </Link> </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
+          <Nav className=" me-auto">
+            <Nav.Link>Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
