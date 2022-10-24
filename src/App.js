b9d8c35622b1e53d7d12523649c02675034a1418
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-10-24 22:38:14 +0600

    header update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import {router} from './Router/Router'
+
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
