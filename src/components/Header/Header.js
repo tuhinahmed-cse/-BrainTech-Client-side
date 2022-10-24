fc967cb92ef098d4ae4edd72fd5ac7303e1bed6a
tuhinahmed-cse <tuhinahmed.cse@gmail.com>
2022-10-24 23:53:54 +0600

    working in header home route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,20 +14,12 @@ const Header = () => {
         <Navbar.Brand ><Link to='/' style={{textDecoration:'none'}} ><FaBrain style={{color:'tomato', height:'30px'}}></FaBrain> <span style={{color:'goldenrod'}}>Brain</span><span className='text-info'>Tech</span> </Link> </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className=" me-auto">
-            <Nav.Link>Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav className=" me-auto"  >
+            <Nav.Link style={{color:'goldenrod'}} >Courses</Nav.Link>
+            <Nav.Link style={{color:'goldenrod'}}>FAQ</Nav.Link>
+            <Nav.Link style={{color:'goldenrod'}}>Blog</Nav.Link>
+            <Nav.Link style={{color:'goldenrod'}}>Toggle</Nav.Link>
+            
           </Nav>
           <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
